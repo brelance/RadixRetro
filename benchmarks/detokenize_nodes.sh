a68de66b47a5f5ee1401e83509cf6bd679b301b7
w11nd <w11nd@qq.com>
2025-12-10 06:40:26 +0000

    test: shell script for runing test

---
+++ benchmarks/detokenize_nodes.sh
@@ -0,0 +1,11 @@
+# node_ids="96,95,97"
+node_ids="2"
+trace_file="/HOME/nju_mli/nju_mli_1/junjie/RadixRetro/traces/tree_node_trace_30.json"
+tokenizer_path="/HOME/nju_mli/nju_mli_1/HDD_POOL/dpskv2"
+
+python3 benchmarks/query_radix_nodes.py \
+  --trace "$trace_file" \
+  --node-ids "$node_ids" \
+  --detokenize \
+  --tokenizer-path "$tokenizer_path" \
+  --skip-special-tokens
